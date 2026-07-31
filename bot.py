9909ed18d565ab915df16acc8d61b2fb5db86657
Mahdiabedi5421 <101071024+Mahdiabedi5421@users.noreply.github.com>
2026-07-31 05:07:35 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,9 @@ from telegram.ext import Application, CommandHandler, MessageHandler, ContextTyp
 TOKEN = os.getenv("BOT_TOKEN")
 
 SELL_URL = "https://divar.ir/s/isfahan/buy-apartment"
-RENT_URL = "https://divar.ir/s/isfahan/rent-residential/shahrak-milad?business-type=personal%2C&districts=1443%2C1444%2C1446%2C1467%2C1605%2C1606%2C2387&map_bbox=51.652939%2C32.650398%2C51.721099%2C32.735992&map_place_hash=4%7C1442%2C1443%2C1444%2C1467%2C1605%2C1606%2C2387%7Capartment-sell%7C"
+
+RENT_URL = "https://divar.ir/s/isfahan/rent-residential/shahrak-milad?business-type=personal%2C&districts=1443%2C1444%2C1467%2C1605%2C1606%2C2387&map_bbox=51.652939%2C32.650398%2C51.721099%2C32.735992"
+
 SUPPORT_PHONE = "09944032954"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -17,7 +19,10 @@ keyboard = [
 await update.message.reply_text(
     "🏠 ربات یابنده املاک\n\n"
     "یکی از گزینه‌های زیر را انتخاب کن:",
-    reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    reply_markup=ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True
+    )
 )
 
 async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -28,9 +33,12 @@ if text == "🏠 فروش":
 
     await update.message.reply_text(
         "🏠 فایل‌های فروش\n\n"
-        "برای مشاهده آگهی‌های فروش روی لینک زیر بزن:\n\n"
+        "لینک آگهی‌های فروش:\n\n"
         SELL_URL,
-        reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard,
+            resize_keyboard=True
+        )
     )
     return
 
@@ -39,9 +47,12 @@ if text == "🏠 اجاره منطقه ۷":
 
     await update.message.reply_text(
         "🏠 رهن و اجاره منطقه ۷\n\n"
-        "برای مشاهده آگهی‌های اجاره روی لینک زیر بزن:\n\n"
+        "لینک آگهی‌های اجاره:\n\n"
         RENT_URL,
-        reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard,
+            resize_keyboard=True
+        )
     )
     return
 
@@ -51,7 +62,10 @@ if text == "📞 تماس و پشتیبانی":
     await update.message.reply_text(
         "📞 تماس و پشتیبانی\n\n"
         "شماره تماس: " + SUPPORT_PHONE,
-        reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard,
+            resize_keyboard=True
+        )
     )
     return
 
@@ -64,7 +78,10 @@ if text == "↩️ برگشت":
     await update.message.reply_text(
         "🏠 منوی اصلی\n\n"
         "یکی از گزینه‌ها را انتخاب کن:",
-        reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard,
+            resize_keyboard=True
+        )
     )
     return
 
